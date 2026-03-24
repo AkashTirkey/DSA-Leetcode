b2d8c42a581e5535dda8ac792f3214039537c1e9
Akashdeep Tirkey <akashdeeptirkey57@gmail.com>
2026-03-24 21:35:37 +0530

    Time: 0 ms (100.00%) | Memory: 45.2 MB (60.50%) - LeetSync

--- 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.cpp
+++ 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.cpp
@@ -1,40 +1,38 @@
 class Solution {
 public:
     int maxAbsoluteSum(vector<int>& nums) {
-        //brute force approach
-        /*int maxSum = INT_MIN;
-        for(int i=0; i<nums.size(); i++){
-            int sum = 0;
-            for(int j=i; j<nums.size(); j++){
-                sum+=nums[j];
+        //brute force approach with T.C-O(n^2)
+        // int maxSum = INT_MIN;
+        // for(int i= 0; i<nums.size(); i++){
+        //     int currentSum=0;
+        //     for(int j=i; j<nums.size(); j++){
+        //         currentSum+=nums[j];
+        //         maxSum = max(maxSum, abs(currentSum));
+        //     }
+        // }
+        // return maxSum;
 
-                maxSum = max(maxSum, abs(sum));
-            }
-        }
-        return maxSum;//time complexity - O(n^2)*/
-
-        //optimal approach with kadane's algo
+        //optimal approach with kadane's Algo Time complexity - O(n).
         int maxSum = INT_MIN;
         int minSum = INT_MAX;
         int currentSum = 0;
-
         for(int i=0; i<nums.size(); i++){
             currentSum+=nums[i];
-            maxSum = max(maxSum , currentSum);
-            if(currentSum < 0) currentSum = 0; //reset if sum becomes negative
+            maxSum=max(maxSum,currentSum);
 
+            if(currentSum<0) currentSum=0;
         }
 
-
-        //kadane's algo inverted 
-        currentSum = 0; //reset for minSum
+        currentSum = 0;
         for(int i=0; i<nums.size(); i++){
             currentSum+=nums[i];
-            minSum = min(minSum, currentSum);
 
-            if(currentSum > 0) currentSum = 0; //reset if sum becomes positive
+            minSum = min(minSum,currentSum);
+
+            if(currentSum>0) currentSum=0;
         }
 
         return max(maxSum, abs(minSum));
+        
     }
 };
