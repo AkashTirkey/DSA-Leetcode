092fa1100e07f1ddbf61a0c1cc7293ba1c1057da
Akashdeep Tirkey <akashdeeptirkey57@gmail.com>
2026-03-25 00:58:07 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (39.26%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -2,14 +2,27 @@ class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
         //brute force approach time complexity - O(n);
-        int idx = 0;
+        /*int idx = 0;
         for(int i=1; i<nums.size(); i++){
             if(nums[i]> nums[i-1]){
                 idx = i;
             }
          }
-         return idx;
+         return idx;*/
 
          //optimal approach - binary search Time complexity - O(logn)
-    }
+         int n = nums.size();
+        int left = 0;
+        int right = n - 1;
+
+        while(left < right){
+            int mid = left + (right - left) / 2;
+
+            if(nums[mid] < nums[mid+1])
+                left = mid + 1;   // peak on right
+            else
+                right = mid;      // ← mid not mid-1 !
+                }
+            return left;  // left == right == peak 
+}
 };
