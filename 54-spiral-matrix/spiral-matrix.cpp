aeb84a95a64983a8780265b66d8fb2f566be4a57
Akashdeep Tirkey <akashdeeptirkey57@gmail.com>
2026-03-30 00:00:30 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (80.90%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -2,47 +2,46 @@ class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
         vector<int> result;
-        int n = matrix.size(); // row size
-        int m=matrix[0].size(); //col size
-        int top = 0, bottom = n-1;
-        int left = 0, right= m-1;
+        int n = matrix.size();
+        int m=matrix[0].size();
+        int top = 0, bottom=n-1;
+        int left = 0, right = m-1;
 
         if(matrix.empty() || matrix[0].empty()) return result;
 
         while(top<= bottom && left <= right){
 
-
-            // 👉sweep top row
-            for(int  j=left; j<= right; j++){
+            //sweep top row
+            for(int j=left; j<=right; j++){
                 result.push_back(matrix[top][j]);
             }
-                top++;
+            top++;
 
-            // 👇sweep right col
-            for(int i = top; i<=bottom; i++){
+
+            //sweep right col
+            for(int i=top; i<=bottom; i++){
                 result.push_back(matrix[i][right]);
             }
             right--;
 
-
-        //👈sweep bottom row
-        if(top <= bottom) {
-            for(int j=right; j>=left; j--){
-                result.push_back(matrix[bottom][j]);
+            //bottom row
+            if(top<=bottom) {
+                for(int j=right; j>=left; j--){
+                    result.push_back(matrix[bottom][j]);
+                }
+                bottom--;
             }
-            bottom--;
-        }
 
-        //sweep left column 👆
-         if(left <= right){
-            for(int i=bottom; i>=top; i--){
-                result.push_back(matrix[i][left]);
+            //left col
+            if(left<=right){
+                for(int i=bottom; i>=top; i--){
+                    result.push_back(matrix[i][left]);
+                }
+                left++;
             }
-            left++;
         }
 
-        }
-        return result; 
-        //Time complexity - O(N x M space complexity - O(1)
+        return result;
+        //Time complexity - O(n x m) space complexity - O(1)
     }
 };
