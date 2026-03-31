0765de5b1422f527f2be3ecf0f8271e94276ece7
Akashdeep Tirkey <akashdeeptirkey57@gmail.com>
2026-03-31 18:51:58 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (92.44%) - LeetSync

--- 898-transpose-matrix/transpose-matrix.cpp
+++ 898-transpose-matrix/transpose-matrix.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
     vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+        //if the matrix is a square matrix
         int n = matrix.size();
         int m = matrix[0].size();
-        
-        //if the matrix is square 
         /*for(int i=0; i<n; i++){
             for(int j=i+1; j<m; j++){
                 swap(matrix[i][j], matrix[j][i]);
             }
         }
-        return matrix;*/
-        //Time complexity - O(n^2) space complexity -  O(1).
+        return matrix; */
+        //time complexity - O(n^2) space - O(1)
 
-        //if matrix is not square 
-
+        //if not a square matrix
         vector<vector<int>> result(m, vector<int>(n));
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
                 result[j][i] = matrix[i][j];
             }
         }
-        return result; 
-        //time complexity - O(nxm) spacecomplexity - O(n)
-
+        return result;
+        //time complexity - O(n x m) space complexity - O(m)
     }
 };
