ee02c3870fa7972e9ec4f593d9870a3fec6c0c84
Akashdeep Tirkey <akashdeeptirkey57@gmail.com>
2026-04-03 00:20:30 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (94.18%) - LeetSync

--- 3543-count-substrings-that-satisfy-k-constraint-i/count-substrings-that-satisfy-k-constraint-i.cpp
+++ 3543-count-substrings-that-satisfy-k-constraint-i/count-substrings-that-satisfy-k-constraint-i.cpp
@@ -1,25 +1,43 @@
 class Solution {
 public:
     int countKConstraintSubstrings(string s, int k) {
-        //brute force approach t.c - O(n^2) space - O(1);
+        // int n = s.size();
+        // int res = 0;
+        // for(int i=0; i<n; i++){
+        //     int count0 = 0;
+        //     int count1 = 0;
+        //      for(int j=i; j<n; j++){
+        //         if(s[j] == '0') count0++;
+        //         else count1++;
+
+        //         if(count0>k && count1>k) break;
+        //         res++;
+        //      }
+        // }
+        // return res;
+        //time complexity - O(n^2);
+
+        //optimal soluition with sliding window
         int n = s.size();
-        int result = 0;
-        for(int i=0; i<n; i++){
-            int count1 = 0;
-            int count0 = 0;
-            for(int j=i; j<n; j++){
+        int left = 0;
+        int res = 0;
+        int count0 = 0;
+        int count1 = 0;
+        for(int right = 0; right<n; right++){
 
-                if(s[j] == '0') count0++;
-                else count1++;
-                
-                if(count0>k && count1>k) break;
+            if(s[right] == '0')count0++;
+            else count1++;
 
-                result++;
-                }
+            //shrink window if both exceed k
+            while(count0 > k && count1 > k){
+                if(s[left] == '0') count0--;
+                else count1--;
+                left++;
             }
-                return result;
-
-                //optimal approach by sliding window
-               
+            
+            res+= (right-left +1);
+        }
+        return res;
+        //time complexity - O(n) space complexity - O(1)
     }
 };
