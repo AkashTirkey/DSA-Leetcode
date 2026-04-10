4659c9bc22c1d75e983f4f545bc2e18d9214c95c
Akashdeep Tirkey <akashdeeptirkey57@gmail.com>
2026-04-10 12:18:15 +0530

    Time: 0 ms (100.00%) | Memory: 32 MB (97.18%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
@@ -1,43 +1,43 @@
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
-        //T.C = O(n^2) brute force
         // int minLen = INT_MAX;
-        // for(int i=0; i<nums.size(); i++){
+        // for(int i=0;i<nums.size(); i++){
         //     int sum = 0;
         //     for(int j=i; j<nums.size(); j++){
-        //         sum+= nums[j];
+        //         sum+=nums[j];
 
-        //         if(sum >= target){
-        //             minLen = min(minLen , j-i+1);
-                         // break;
+        //         if(sum == target){
+        //             minLen = min(minLen,j-i+1);
+        //             break;
         //         }
+
         //     }
         // }
         // if(minLen == INT_MAX) return 0;
         // else return minLen;
 
-        //Optimal Approach with sliding window
+        //optimal approach using sliding window 
 
         int left = 0;
         int minLen = INT_MAX;
         int sum = 0;
-        for(int right = 0; right<nums.size(); right++){
+        for(int right; right<nums.size(); right++){
             sum+=nums[right];
+
             while(sum >= target){
+                sum= sum - nums[left];
+            
                 //update the minLength
                 minLen = min(minLen,right-left+1);
-                //decreement the left ele from sum
-                sum-=nums[left];
+                
                 //increase the left pointer
                 left++;
             }
 
         }
-
         if(minLen == INT_MAX) return 0;
         else return minLen;
-
-
+        
     }
 };
