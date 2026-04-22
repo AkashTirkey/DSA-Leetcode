2e9899d9a6471699066ed151b9e1c6c6d6508527
Akashdeep Tirkey <akashdeeptirkey57@gmail.com>
2026-04-23 00:51:05 +0530

    Time: 0 ms (100.00%) | Memory: 26.4 MB (20.77%) - LeetSync

--- 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
@@ -1,12 +1,29 @@
 class Solution {
 public:
     vector<int> getSneakyNumbers(vector<int>& nums) {
+        // vector<int> ans;
+        // for(int i=0; i<nums.size()-1; i++){
+        //     for(int j=i+1; j<nums.size(); j++){
+        //         if(nums[i] == nums[j]) ans.push_back(nums[i]);
+        //     }
+        // }
+        // return ans;
+        //TIme complexity - O(n^2) Space complexity - O(n);
+
+        //optimised Approach
         vector<int> ans;
-        for(int i=0; i<nums.size()-1; i++){
-            for(int j=i+1; j<nums.size(); j++){
-                if(nums[i] == nums[j]) ans.push_back(nums[i]);
+        unordered_map<int,int> mp;
+        for(int i=0; i<nums.size(); i++){
+            mp[nums[i]]++;
+        }
+
+        for(auto it:mp){
+            if(it.second>1){
+                ans.push_back(it.first);
             }
         }
         return ans;
+        //Time Complexity - O(n) Space Complexity - O(n);
+
     }
 };
