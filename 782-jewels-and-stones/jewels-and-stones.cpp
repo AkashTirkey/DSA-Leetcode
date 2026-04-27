1997d855b84f123f299914473c4bfd50010ae7ee
Akashdeep Tirkey <akashdeeptirkey57@gmail.com>
2026-04-27 17:12:37 +0530

    Time: 2 ms (18.14%) | Memory: 8.6 MB (47.01%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.cpp
+++ 782-jewels-and-stones/jewels-and-stones.cpp
@@ -1,13 +1,29 @@
 class Solution {
 public:
     int numJewelsInStones(string jewels, string stones) {
-        int output = 0;
-        for(char c : stones){
-            for(char k : jewels){
-                if(c == k) output++;
+        //brute force approach with T.C - O(n^2).
+        // int output = 0;
+        // for(char c : stones){
+        //     for(char k : jewels){
+        //         if(c == k) output++;
+        //     }
+        // }
+        // return output;
+        //Time complexity - O(n^2) Space complexity - O(1).
+
+        //Optimised approach with T.C - O(n)?
+        //using hashmap
+
+        unordered_set<char> jewelSet(jewels.begin(), jewels.end());
+
+        int count = 0 ;
+        for(char c: stones){
+            if(jewelSet.count(c)){
+                count++;
             }
         }
-        return output;
-        //Time complexity - O(n^2) Space complexity - O(1).
+        return count;
+        //Time Complexity -O(n) space Complexity -O(n).
+
     }
 };
