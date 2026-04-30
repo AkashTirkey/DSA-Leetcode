10aeb2bda3675a6a592928858ff54726bc26491a
Akashdeep Tirkey <akashdeeptirkey57@gmail.com>
2026-04-30 12:09:56 +0530

    Time: 8 ms (51.36%) | Memory: 133.5 MB (43.92%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
@@ -1,38 +1,62 @@
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-        vector<int> arr1;
-        vector<int> arr2;
-        vector<int> arr3;
+        // vector<int> arr1;
+        // vector<int> arr2;
+        // vector<int> arr3;
 
-        vector<int> res;
+        // vector<int> res;
 
-        for(int i=0; i<nums.size(); i++){
-            if(nums[i]<pivot){
-                arr1.push_back(nums[i]); //9,5,3
-            }
-            else if(nums[i] == pivot) arr2.push_back(nums[i]); //10, 10
-            else{
-                arr3.push_back(nums[i]); //12,14
-            }
-        }
+        // for(int i=0; i<nums.size(); i++){
+        //     if(nums[i]<pivot){
+        //         arr1.push_back(nums[i]); //9,5,3
+        //     }
+        //     else if(nums[i] == pivot) arr2.push_back(nums[i]); //10, 10
+        //     else{
+        //         arr3.push_back(nums[i]); //12,14
+        //     }
+        // }
 
         
 
-        int i=0,j=0,k=0;
-        while(i<arr1.size()){
-            res.push_back(arr1[i]);
-            i++;
-        }
-        while(j<arr2.size()){
-            res.push_back(arr2[j]);
-            j++;
+        // int i=0,j=0,k=0;
+        // while(i<arr1.size()){
+        //     res.push_back(arr1[i]);
+        //     i++;
+        // }
+        // while(j<arr2.size()){
+        //     res.push_back(arr2[j]);
+        //     j++;
+        // }
+
+        // while(k<arr3.size()) {
+        //     res.push_back(arr3[k]);
+        //     k++;
+        // }
+        // return res;
+
+        //time complexity - O(n) + O(arr1) + O(arr2) + O(arr3) space complexity - O(n)
+
+        //optimsed approach with less loops 
+        vector<int> res;
+
+        for(int x : nums){
+            if(x<pivot) res.push_back(x);
         }
 
-        while(k<arr3.size()) {
-            res.push_back(arr3[k]);
-            k++;
+        for(int x : nums){
+            if(x == pivot) res.push_back(x);
         }
+
+        for(int x:nums){
+            if(x > pivot) res.push_back(x);
+        }
+
         return res;
+        //Time complexity - O(n) space complexity - O(n);
+
+
+
+
     }
 };
