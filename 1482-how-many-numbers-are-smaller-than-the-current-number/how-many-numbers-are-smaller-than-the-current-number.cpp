53dd08220ae0078fdfa3204f6843a7ccc87df5d1
Akashdeep Tirkey <akashdeeptirkey57@gmail.com>
2026-05-06 16:55:02 +0530

    Time: 0 ms (100.00%) | Memory: 14.6 MB (14.99%) - LeetSync

--- 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.cpp
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.cpp
@@ -1,17 +1,35 @@
 class Solution {
 public:
     vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+        // vector<int> ans;
+        // for(int i=0; i<nums.size(); i++){
+        //     int count=0;
+        //     for(int j=0; j<nums.size(); j++){
+        //         if(nums[j]<nums[i]){
+        //             count++;
+        //         }
+        //     }
+        //     ans.push_back(count);
+        // }
+        // return ans;
+        //Time Complexity-O(n^2) Space Complexity - O(n).
+
+        //optimised approach with mapping + sorting
+        vector<int> temp = nums;
+        sort(temp.begin(), temp.end()); //O(nlogn)
+
+        unordered_map<int,int> mp;
+        for(int i=0; i<temp.size(); i++){
+            if(mp.find(temp[i])==mp.end()){
+                mp[temp[i]] = i;
+            }
+        }
+
         vector<int> ans;
         for(int i=0; i<nums.size(); i++){
-            int count=0;
-            for(int j=0; j<nums.size(); j++){
-                if(nums[j]<nums[i]){
-                    count++;
-                }
-            }
-            ans.push_back(count);
+            ans.push_back(mp[nums[i]]);
         }
         return ans;
-        //Time Complexity-O(n^2) Space Complexity - O(n).
+        //Time Complexity- O(nlogn) Space complexity - O(n).
     }
 };
