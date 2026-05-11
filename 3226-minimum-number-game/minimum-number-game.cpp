2dc101d0e7a8e2bc1e0448ac2d96f09e6ba6d9bd
Akashdeep Tirkey <akashdeeptirkey57@gmail.com>
2026-05-11 14:14:43 +0530

    Time: 0 ms (100.00%) | Memory: 28.4 MB (32.10%) - LeetSync

--- 3226-minimum-number-game/minimum-number-game.cpp
+++ 3226-minimum-number-game/minimum-number-game.cpp
@@ -1,14 +1,25 @@
 class Solution {
 public:
     vector<int> numberGame(vector<int>& nums) {
-        // vector<int> arr; //empty arr for storing elements.
-
-        sort(nums.begin(), nums.end()); //O(nlogn);
-
-        for(int i=0; i<nums.size(); i+=2){
-            swap(nums[i],nums[i+1]);
+        vector<int> arr; //empty arr for storing elements.
+        sort(nums.begin(),nums.end());
+        for(int i=0; i<nums.size()-1; i+=2){
+            for(int j=i+1; j<nums.size(); j++){
+                arr.push_back(nums[j]);
+                arr.push_back(nums[i]);
+                break;
+            }
         }
-        return nums;
-        //T.C - O(n) S.C-O(1).
+        return arr; 
+        //T.C - O(nlogn) + O(n^2) S.c - O(n);
+        
+        //optimised approach
+        // sort(nums.begin(), nums.end()); //O(nlogn);
+
+        // for(int i=0; i<nums.size(); i+=2){
+        //     swap(nums[i],nums[i+1]);
+        // }
+        // return nums;
+        // //T.C - O(n) S.C-O(1).
     }
 };
