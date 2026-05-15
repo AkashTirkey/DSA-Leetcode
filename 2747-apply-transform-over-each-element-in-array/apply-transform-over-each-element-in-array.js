6fc70a61d663ef279840620cae848c5944dd92d5
Akashdeep Tirkey <akashdeeptirkey57@gmail.com>
2026-05-15 12:13:48 +0530

    Time: 46 ms (40.24%) | Memory: 52.6 MB (95.02%) - LeetSync

--- 2747-apply-transform-over-each-element-in-array/apply-transform-over-each-element-in-array.js
+++ 2747-apply-transform-over-each-element-in-array/apply-transform-over-each-element-in-array.js
@@ -4,17 +4,9 @@
  * @return {number[]}
  */
 var map = function(arr, fn) {
-    // const transformedArr = [];
-    // for(let i=0; i<arr.length; i++){
-    //     transformedArr[i] = fn(arr[i],i);
-    // }
-    // return transformedArr;
-    //T.C & S.C - O(n).
-
-    //optimised approach
+    let transformedArr = [];
     for(let i=0; i<arr.length; i++){
-        arr[i] = fn(arr[i],i);
+        transformedArr[i] = fn(arr[i],i);
     }
-    return arr;
-    //S.C - O(1) T.C-O(n)
+    return transformedArr;
 };
