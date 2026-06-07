d7c7fc3ea3164fab755713b80d8d6ef71d9301d3
Akashdeep Tirkey <akashdeeptirkey57@gmail.com>
2026-06-07 20:30:16 +0530

    Time: 1 ms (54.05%) | Memory: 9.9 MB (10.88%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,28 +1,39 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        // for(int i=0; i<s.size(); i++){
+        //2 strings are anagram of each other when 
+        //a) They contain the same characters.
+        //b) Each character appears the same number of times.
+        // int n=s.size();
+        // int m=t.size();
+        
+        // for(int i=0; i<n; i++){
         //     int countS = 0;
-        //     int countT=0;
-        //     for(int j=0; j<s.size(); j++){
-        //         if(s[j] == s[i]) countS++;
-        //         if(t[j] == s[i]) countT++;
+        //     int countT = 0;
+        //     for(int j=0; j<m; j++){
+        //         if(s[j]==s[i]) countS++;
+        //         if(t[j]==s[i]) countT++;
         //     }
         //     if(countS!=countT) return false;
         // }
         // return true;
+        //Time Complexity - O(n^2) & space complexity - O(1)
 
-        //optimal approach using hashmap or array
-        if(s.size()!= t.size()) return false;
-        int freq[26] = {0};
-        for(int i=0; i<s.size(); i++){
-            freq[s[i]-'a']++;
-            freq[t[i]-'a']--;
+        //optimal approach using hashmap
+        unordered_map<char,int> mp;
+
+        for(char ch:s){
+            mp[ch]++;
         }
 
-        for(int i=0; i<26; i++){
-            if(freq[i]!=0) return false;
+        for(char ch:t){
+            mp[ch]--;
+        }
+
+        for(auto it : mp){
+            if(it.second != 0) return false;
         }
         return true;
+
     }
 };
