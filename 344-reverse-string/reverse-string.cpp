a6f2173f7262786e7f1c7d0b4982215d525cc06a
Akashdeep Tirkey <akashdeeptirkey57@gmail.com>
2026-06-08 23:59:58 +0530

    Time: 0 ms (100.00%) | Memory: 27.1 MB (98.07%) - LeetSync

--- 344-reverse-string/reverse-string.cpp
+++ 344-reverse-string/reverse-string.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
+    void swap(vector<char>&s,int l,int r){
+        char temp = s[l];
+        s[l] = s[r];
+        s[r] = temp;
+    }
     void reverseString(vector<char>& s) {
         int n = s.size();
         int left = 0;
         int right = n-1;
 
-        //since it is asked to do in place so two pointers is the best approach for this
-        while(left<=right){
-            swap(s[left],s[right]);
+        while(left<right){
+            swap(s,left,right);
             left++;
             right--;
         }
