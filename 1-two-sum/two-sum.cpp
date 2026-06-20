ffbe5543f9ef97f4bc79f5a8f3403e96d336321c
Akashdeep Tirkey <akashdeeptirkey57@gmail.com>
2026-06-20 16:30:51 +0530

    Time: 0 ms (100.00%) | Memory: 14.8 MB (54.49%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,16 +1,38 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        for(int i=0; i<nums.size(); i++){
-            // int sum=0;
-            for(int j=i+1; j<nums.size(); j++){
+        // for(int i=0; i<nums.size(); i++){
+        //     for(int j=i+1; j<nums.size(); j++){
                 
-                if(nums[i] + nums[j] == target){
-                    return {i,j};
-                }
-            }
-        }
-        return {-1,-1};
+        //         if(nums[i] + nums[j] == target){
+        //             return {i,j};
+        //         }
+        //     }
+        // }
+        // return {-1,-1};
         //Time complexity - O(N^2) Space Complexity - O(1);
+
+        //Optimal Approach using hashmap
+        unordered_map<int,int> mp;
+        
+        // for(int i=0; i<nums.size(); i++){
+        //     mp[nums[i]] = i;
+        // // 2 ->0 
+        // // 7->1
+        // // 11->2
+        // // 15->3
+        // } 
+
+        for(int i=0; i<nums.size(); i++){
+            int complement = target-nums[i];
+
+            if(mp.count(complement)){
+                return {i,mp[complement]};
+            }
+
+            else mp[nums[i]] = i;
+        }
+    return {-1,-1};
     }
+    //T.C - O(n) S.C - O(n)
 };
