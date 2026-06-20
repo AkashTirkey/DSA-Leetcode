de0fed25ed7936be7261695bea5c0c2a14d1085c
Akashdeep Tirkey <akashdeeptirkey57@gmail.com>
2026-06-20 17:09:54 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (32.98%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -1,33 +1,35 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-        int n = numbers.size();
-        /*for(int i=0; i<n; i++){
-            for(int j=i+1; j<n; j++){
-                if(numbers[j] + numbers[i] == target){
-                    return {i+1,j+1};
-                }
-            }
-        }
-        return {0,0};*/
-        //time complxity - O(n^2) space complexity - O(1);
+        // int n=numbers.size();
+        // for(int i=0; i<n; i++){
+        //     for(int j=i+1; j<n; j++){
+        //         if(numbers[i] + numbers[j] == target){
+        //             return {i+1,j+1};
+        //         }
+        //     }
+        // }
+        // return {-1,-1};
+        //Time complexity - O(n^2) space complexity-O(1)
 
-        //optimal approach with Two pointers
+        //optimal solution using Two pointers since solution is needed to be O(1) space complexity
         int left = 0;
+        int n=numbers.size();
         int right = n-1;
-        while(left < right){
+
+        while(left< right){
             if(numbers[left] + numbers[right] == target){
                 return {left+1, right+1};
             }
 
-            else if(numbers[left] + numbers[right] > target){
+            else if(numbers[left] + numbers[right]  > target){
                 right--;
             }
-            else {
-                left++;
-            }
+
+            else left++;
         }
-        return {0,0};
-        //Time complexity - O(n) space complexity - O(1)
+        return {-1,-1};
+        //Time complexity-O(n) S.C - O(1);
+
     }
 };
