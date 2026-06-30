68a08921c25e5b82743d8391dcd77cd519a007ef
Akashdeep Tirkey <akashdeeptirkey57@gmail.com>
2026-06-30 22:57:19 +0530

    Time: 8 ms (33.96%) | Memory: 10.7 MB (25.77%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -1,58 +1,57 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-       /* int k = s1.size();
+        // int k=s1.size();
+
+        // //frequency map for s1
+        // unordered_map<char,int> mp1;
+        // for(int i=0; i<k; i++){
+        //     mp1[s1[i]]++; //a->1 b->1
+        // }
+
+        // for(int i=0; i<s2.size()-k; i++){
+        //     //create new hashmap for each substring
+        //     unordered_map<char,int> mp2;
+        //     for(int j=i; j<i+k; j++){
+                
+        //         //Count frequncy of characters in substring
+        //         mp2[s2[j]]++;
+                
+        //         //compare it with mp1
+        //         if(mp1 == mp2) return true;
+        //     }
+        // }
+        // return false;
+
+        // T.C - O(k) + O(n*k) 
+        // S.C - O(n)
+
+        //optimal solution using sliding window
         unordered_map<char,int> mp1;
-        
-        //frequency map for s1
-        for(int i=0; i<k; i++){
+        for(int i=0; i<s1.size(); i++){
             mp1[s1[i]]++;
         }
-        
 
-        //check if mp1 is present in s2
-        for(int i=0; i<=s2.size()-k; i++){
-            //create new hashmap for new elements of k size
-            unordered_map<char,int> mp2;
-            for(int j=i; j<i+k; j++){
-                mp2[s2[j]]++;
-                }
-                if(mp1 == mp2){
-                    return true;
-                }
-            }
-
-        return false; // T.C-O(n) + O(n+k)*/
-
-        //optimal solution sliding window
-        int k = s1.size();
-        unordered_map<char,int> mp1;
-
-        for(int i=0; i<k; i++){
-            mp1[s1[i]]++;
-        }
-
+        // step2: create first window in s2
         unordered_map<char,int> mp2;
-        int left = 0;
-        for(int right=0; right<s2.size(); right++){
-           mp2[s2[right]]++; // add new char
+            int left = 0;
+            for(int right = 0; right<s2.size(); right++){
+                mp2[s2[right]]++; //add new char
+                
 
-           //window size check
-           if(right - left + 1 > k){
-                mp2[s2[left]]--;
-                if(mp2[s2[left]] == 0){
-                    mp2.erase(s2[left]);
+                //window size check
+                if(right-left + 1 > s1.size()){
+                    mp2[s2[left]]--;
+                    if(mp2[s2[left]] == 0){
+                        mp2.erase(s2[left]);
+                    }
+                    left++;
+
                 }
-                left++;
-           }
-
-           //compare both maps
-           if(mp1 == mp2) return true;
-            
+                    //compare both maps
+                    if(mp1 == mp2) return true;
             }
-        return false; //T.c - O(n);
-        
 
-
+            return false;
     }
 };
